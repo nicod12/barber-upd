3ca16f7bc14691ce356e9ad274623a4068926a13
dumenicolas45@gmail.com <nicolas@DESKTOP-DAMTQ93.>
2024-10-30 05:45:12 -0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,15 @@ import CustomForm from "@/components/CurstomForm";
 import Footer from "@/components/Footer";
 import Gallery from "@/components/Gallery";
 import Hero from "@/components/Hero";
-import MapComponent from "@/components/MapComponent";
 
 
 export default function Home() {
+
+
   return (
     <div>
       <Hero />
       <Gallery />
-      <MapComponent />
       <CustomForm />
       <Footer />
     </div>
